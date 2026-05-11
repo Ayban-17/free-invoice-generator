6ac95d480a82519114c8f85f9da7bd733e9bf35e
Ayban-17 <mrkvnhrnndz17@gmail.com>
2026-05-12 01:31:31 +0800

    add sitemap in config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import Sitemap from 'vite-plugin-sitemap'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    Sitemap({
+      hostname: 'https://free-invoice-generator-ph.netlify.app',
+      dynamicRoutes: ['/generator'],
+      readable: true,
+      changefreq: 'monthly',
+      priority: 1.0,
+      lastmod: new Date(),
+    }),
+  ],
 })
